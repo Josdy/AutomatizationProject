942123d9ef88f0a5b96a031b5f2eb77a2732cc17
Josdy <josdy96@hotmail.com>
2020-05-18 20:25:51 -0500

    --

--- SISOSECAutomatizationTest/src/test/java/com/selenium/page/HomePage.java
+++ SISOSECAutomatizationTest/src/test/java/com/selenium/page/HomePage.java
@@ -1,5 +1,6 @@
 package com.selenium.page.bk;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -33,6 +34,19 @@ public class HomePage extends BaseConfig
   private WebElement loginbtn;
 
   //create method
+<<<<<<< HEAD:SISOSECAutomatizationTest/src/test/java/com/selenium/page/HomePage.java
+  public void LogIn(String user, String pass) {
+		username.sendKeys(user);
+		nextbtn.click();
+		WaitWebDriver.waitVisibility(driver, password, 5);
+		password.sendKeys(pass, Keys.ENTER);
+		//WaitWebDriver.waitClickable(driver, loginbtn, 8);
+		//loginbtn.click();
+		
+	}
+
+  
+=======
   public void LogIn(String user, String pass)
   {
     username.sendKeys(user);
@@ -44,4 +58,5 @@ public class HomePage extends BaseConfig
 
   }
 
+>>>>>>> da702dc068dc91460e10edb4c70f7fc0ec35b006:SISOSECAutomatizationTest/src/test/java/com/selenium/page/bk/HomePage.java
 }
